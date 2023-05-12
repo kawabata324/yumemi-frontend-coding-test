478319f7e5b1d6175890589b85a3c7e9eb83252e
kawabata324 <kawabataharuki48@gmail.com>
2023-05-13 05:12:44 +0900

    :sparkles: titleの追加

--- src/components/presentational/prefCheckboxes/PrefCheckboxes.tsx
+++ src/components/presentational/prefCheckboxes/PrefCheckboxes.tsx
@@ -10,6 +10,7 @@ type Props = {
 }
 export const PrefCheckboxes: FC<Props> = ({ onChange, checkedIdList, prefList }) => (
   <div>
+    <h2 className="pref_checkboxes--title">都道府県</h2>
     <ul className="pref_checkboxes--pref_checkboxes">
       {prefList.map((pref) => (
         <li key={pref.prefCode}>
