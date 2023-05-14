ca7268ebd8adcc49f98b8e2612e074ecde280e1b
kawabata324 <kawabataharuki48@gmail.com>
2023-05-14 20:52:07 +0900

    :art: 外部から初期値を渡してテストしやすくした

--- src/components/container/PopulationTrend.tsx
+++ src/components/container/PopulationTrend.tsx
@@ -1,12 +1,21 @@
 import { FC } from "react"
 import { Trend } from "@/components/presentational/Trend"
 import { useViewModel } from "@/components/container/useViewModel"
+import { ALL_POPULATION } from "@/constants/populationCompositionType"
 
+export const INITIAL_STATE = {
+  initSelectedLabel: ALL_POPULATION,
+  initTotalPopulations: [],
+  initYoungPopulations: [],
+  initWorkingPopulations: [],
+  initOrderPopulations: [],
+  initPrefCodeList: [],
+}
 export const PopulationTrend: FC = () => {
   const {
     state: { prefList, prefCodeList, composition, selectedLabel },
     action: { checkPrefecture, changeComposition },
-  } = useViewModel()
+  } = useViewModel(INITIAL_STATE)
   return (
     <Trend
       prefList={prefList}
