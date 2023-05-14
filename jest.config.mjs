a65f28d817e84be9eb72725f486c1a0503dc8835
kawabata324 <kawabataharuki48@gmail.com>
2023-05-14 21:46:16 +0900

    :wrench: coverage report を 出すための設定を追加

--- jest.config.mjs
+++ jest.config.mjs
@@ -10,7 +10,8 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
+  collectCoverage: true,
+  coverageDirectory: "coverage",
   testEnvironment: "jest-environment-jsdom",
 }
 
