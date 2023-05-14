773843e72e6f3a5f9480dcc9f243a746ced4c487
kawabata324 <kawabataharuki48@gmail.com>
2023-05-14 11:30:12 +0900

    :sparkles: 人口構成を取ってくるAPIを作成

---
+++ src/components/hooks/api/usePopulationComposition.test.tsx
@@ -0,0 +1,45 @@
+import { resasApi } from "@/libs/axios"
+import { renderHook } from "@testing-library/react"
+import { usePopulationComposition } from "@/components/hooks/api/usePopulationComposition"
+import {
+  populationByOlderApiFixture,
+  populationByWorkingAgeApiFixture,
+  populationByYoungerApiFixture,
+  populationCompositionApiFixture,
+  totalPopulationApiFixture,
+} from "@/test/fixtures/populationComposition/populationCompositionApiFixture"
+import { resasResponse403Error } from "@/test/fixtures/resusResponseErrorFixture"
+
+describe("usePopulationComposition", () => {
+  describe("fetchPopulationComposition", () => {
+    it("正常系: データが取得できること", async () => {
+      jest.spyOn(resasApi, "get").mockResolvedValue({
+        data: populationCompositionApiFixture,
+      })
+
+      const { result } = renderHook(() => usePopulationComposition())
+      const res = await result.current.action.fetchPopulationComposition(13)
+      expect(res).toMatchObject({
+        totalPopulation: totalPopulationApiFixture.data,
+        populationByYounger: populationByYoungerApiFixture.data,
+        populationByWorking: populationByWorkingAgeApiFixture.data,
+        populationByOlder: populationByOlderApiFixture.data,
+      })
+    })
+    it("異常系: データが取得できないこと", async () => {
+      jest.spyOn(resasApi, "get").mockResolvedValue({
+        data: resasResponse403Error,
+      })
+      jest.spyOn(console, "error").mockImplementation(() => {})
+
+      const { result } = renderHook(() => usePopulationComposition())
+      const res = await result.current.action.fetchPopulationComposition(13)
+      expect(res).toMatchObject({
+        totalPopulation: [],
+        populationByYounger: [],
+        populationByWorking: [],
+        populationByOlder: [],
+      })
+    })
+  })
+})
