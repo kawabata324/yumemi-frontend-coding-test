da0ad787e8bd755aaaa00bb3ebeb05e36531d48c
kawabata324 <kawabataharuki48@gmail.com>
2023-05-19 21:56:34 +0900

    :sparkles: cursor: pointerの追加

--- src/stories/PrefCheckbox.stories.ts
+++ src/stories/PrefCheckbox.stories.ts
@@ -1,4 +1,7 @@
+import { expect } from "@storybook/jest"
 import { Meta, StoryObj } from "@storybook/react"
+import { userEvent, within } from "@storybook/testing-library"
+import { selectors } from "playwright-core"
 
 import { PrefCheckbox } from "@/components/presentational/PrefCheckbox"
 
@@ -22,5 +25,19 @@ export const Checked: Story = {
     prefName: "青森県",
     prefCode: 2,
     checkedIdList: [2, 3],
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const checkboxInput = canvas.getByLabelText("青森県", {
+      selector: "input",
+    })
+    const checkBoxLabel = canvas.getByText("青森県")
+
+    await userEvent.hover(checkboxInput)
+    await userEvent.hover(checkBoxLabel)
+    expect(checkboxInput).toHaveStyle("cursor: pointer")
+    expect(checkBoxLabel).toHaveStyle("cursor: pointer")
   },
 }
